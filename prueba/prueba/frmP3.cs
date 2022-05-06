1569f7af3cb23d9387e6b30d8dce3e199c20884b
00218621 <00218621@uca.edu.sv>
2022-05-05 21:08:51 -0600

    :David:Prueba

---
+++ prueba/prueba/frmP3.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace prueba
+{
+    public partial class frmP3 : Form
+    {
+        public frmP3()
+        {
+            InitializeComponent();
+        }
+    }
+}
